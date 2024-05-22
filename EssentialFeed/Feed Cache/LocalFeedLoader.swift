209c8d6de9be938d878e301c1ca0ba6615aff4b1
Nikolay Riskov <riskoni>
2024-05-23 00:37:26 +0300

    Load command delivers cached images on less than seven days old cache

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -31,11 +31,18 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult)->Void ) {
-        store.retrieve{ error in
-            if let error = error {
+        store.retrieve{ result in
+            switch result {
+            case let .failure(error):
                 completion(.failure(error))
-            }else{
+                
+            case let .found(feed, _):
+                completion(.success(feed.toModels()))
+                break
+                
+            case .empty:
                 completion(.success([]))
+                
             }
         }
     }
@@ -50,9 +57,13 @@ public final class LocalFeedLoader {
 }
 
 private extension Array where Element == FeedImage {
-    
     func toLocal() -> [LocalFeedImage] {
         return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
     }
-    
+}
+
+private extension Array where Element == LocalFeedImage {
+    func toModels() -> [FeedImage] {
+        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    }
 }
--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -12,13 +12,13 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     
     func test_init_doesNotMessageTheStoreUponCreation() {
         let (_, store) = makeSUT()
-
+        
         XCTAssertEqual(store.receivedMessages, [])
     }
     
     func test_load_requestsCacheRetrieval() {
         let (sut, store) = makeSUT()
-
+        
         sut.load() { _ in }
         
         XCTAssertEqual(store.receivedMessages, [.retrieve])
@@ -35,12 +35,23 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     
     func test_load_deliversNoImagesOnEmptyCache() {
         let (sut, store) = makeSUT()
-
+        
         expect(sut, toCompleteWith: .success([])) {
             store.completRetrievalWithEmptyCache()
         }
     }
- 
+    
+    func test_load_deliversCachedImagesOnLessThanSevenDaysOldCache() {
+        let fixedCurrentDate = Date()
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let feed = uniqueImageFeed()
+        
+        expect(sut, toCompleteWith: .success(feed.models)) {
+            store.completRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimestamp)
+        }
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping ()->Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
@@ -58,7 +69,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
             switch (receivedResult, expectedResult) {
             case let (.success(receivedImages), .success(expectedImages)):
                 XCTAssertEqual(receivedImages, expectedImages, file: file, line: line)
-
+                
             case let (.failure(receivedError), .failure(expectedError)):
                 XCTAssertEqual(receivedError as NSError?, expectedError as NSError?, file: file, line: line)
                 
@@ -72,8 +83,33 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    private func uniqueFeed() -> FeedImage {
+        return FeedImage(id: UUID(), description: "any", location: "any", url: anyURL())
+    }
+    
+    private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+        let models = [uniqueFeed(), uniqueFeed()]
+        let local = models.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.description, url: $0.url )}
+        return (models, local)
+    }
+    
     private func anyNSError() -> NSError {
         return NSError(domain: "any error", code: 1)
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
+}
+
+private extension Date {
+    
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
     }
     
 }
