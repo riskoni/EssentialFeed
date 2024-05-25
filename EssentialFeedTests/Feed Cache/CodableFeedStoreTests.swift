b22a02ff9cde9d69473892004decbeb2c5b1944c
Nikolay Riskov <riskoni>
2024-05-25 12:13:23 +0300

    Retrieve delivers failure on retrieval error

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -49,9 +49,13 @@ class CodableFeedStore {
             return
         }
         
-        let decoder = JSONDecoder()
-        let cache = try! decoder.decode(Cache.self, from: data)
-        completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+        do{
+            let decoder = JSONDecoder()
+            let cache = try decoder.decode(Cache.self, from: data)
+            completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+        }catch {
+            completion(.failure(error))
+        }
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
@@ -110,6 +114,14 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
     }
     
+    func test_retrieve_deliversFailureOnRetrievalError() {
+        let sut = makeSUT()
+        
+        try! "Invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+        
+        expect(sut, toRetrieve: .failure(anyNSError()))
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
@@ -139,6 +151,9 @@ class CodableFeedStoreTests: XCTestCase {
         sut.retrieve{ retrievalResult in
             switch (expectedReuslt, retrievalResult) {
             case (.empty, .empty):
+                break
+                
+            case (.failure, .failure):
                 break
                 
             case let (.found(feed: retrievedFeed, timestamp: retrievedTimestamp),
