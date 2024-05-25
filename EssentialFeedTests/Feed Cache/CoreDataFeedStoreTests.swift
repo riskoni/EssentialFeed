33a7c8117f5c27f2bf305929356f32889dc4f79c
Nikolay Riskov <riskoni>
2024-05-25 16:33:30 +0300

    Prove that delete deletes all objects

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -143,21 +143,21 @@ class CoreDataFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
     }
 
     func test_delete_removesAllObjects() {
-//        let store = try makeSUT()
-//
-//        insert((uniqueImageFeed(), Date()), to: store)
-//
-//        deleteCache(from: store)
-//
-//        let context = try NSPersistentContainer.load(
-//            name: CoreDataFeedStore.modelName,
-//            model: XCTUnwrap(CoreDataFeedStore.model),
-//            url: inMemoryStoreURL()
-//        ).viewContext
-//
-//        let existingObjects = try context.allExistingObjects()
-//
-//        XCTAssertEqual(existingObjects, [], "found orphaned objects in Core Data")
+        let store = makeSUT()
+
+        insert((uniqueImageFeed().local, Date()), to: store)
+
+        deleteCache(from: store)
+
+        let context = try! NSPersistentContainer.load(
+            name: CoreDataFeedStore.modelName,
+            model: XCTUnwrap(CoreDataFeedStore.model),
+            url: inMemoryStoreURL()
+        ).viewContext
+
+        let existingObjects = try! context.allExistingObjects()
+
+        XCTAssertEqual(existingObjects, [], "found orphaned objects in Core Data")
     }
 
     func test_storeSideEffects_runSerially() {
---
+++ EssentialFeedTests/Feed Cache/Helpers/NSManagedObjectContext+Helpers.swift
@@ -0,0 +1,24 @@
+//
+//  NSManagedObjectContext+Helpers.swift
+//  EssentialFeedTests
+//
+//  Created by Nikolay on 25.05.24.
+//
+
+import CoreData
+
+extension NSManagedObjectContext {
+    func allExistingObjects() throws -> [NSManagedObject] {
+        guard let model = persistentStoreCoordinator?.managedObjectModel else {
+            return []
+        }
+
+        return try model
+            .entities
+            .compactMap { $0.name }
+            .reduce([NSManagedObject](), { acc, entity in
+                let request = NSFetchRequest<NSManagedObject>(entityName: entity)
+                return try acc + fetch(request)
+            })
+    }
+}
