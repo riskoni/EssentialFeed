fd4772e09c56d77300170e42730ccb1441429bd1
Nikolay Riskov <riskoni>
2024-05-27 23:33:44 +0300

    `LocalFeedLoader` in integration with the `CoreDataFeedStore` delivers no items on empty cache

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -55,7 +55,7 @@ class CodableFeedStoreTests: XCTestCase, FailableFeedStoreSpecs {
         expect(sut, toRetrieve: .failure(anyNSError()))
     }
     
-    func test_retrieve_hasNoSideEffectsOnFailure() {
+    func test_retrieve_hasNoSideEffectsOnFailure() åœ{
         let storeURL = testSpecificStoreURL()
         let sut = makeSUT(storeURL: storeURL)
         
