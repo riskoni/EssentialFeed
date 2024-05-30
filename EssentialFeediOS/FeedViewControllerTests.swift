c9fddecc4671d57d8c62bda2978bda0c335f61ab
Nikolay Riskov <riskoni>
2024-05-30 23:22:07 +0300

    Updating project to recommended settings

---
+++ EssentialFeediOS/FeedViewControllerTests.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewControllerTests.swift
+//  EssentialFeediOS
+//
+//  Created by Nikolay on 30.05.24.
+//
+
+import XCTest
+import UIKit
+
+final class FeedViewControllerTests: XCTestCase {
+    
+}
