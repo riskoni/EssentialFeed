54e4cb85a968c3ff211ca5610fcccd9b2aac877d
Nikolay Riskov <nriskov@abrites.com>
2025-06-02 00:13:14 +0300

    Move FeedImagePresenter to its own file

---
+++ EssentialFeed/Feed Presentation/FeedImagePresenter.swift
@@ -0,0 +1,58 @@
+//
+//  FeedImagePresenter.swift
+//  EssentialFeed
+//
+//  Created by Nikolay Riskov on 2.06.25.
+//
+
+import Foundation
+
+public protocol FeedImageView {
+    associatedtype Image
+
+    func display(_ model: FeedImageViewModel<Image>)
+}
+
+public final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
+    private let imageTransformer: (Data) -> Image?
+
+    private struct InvalidImageDataError: Error {}
+
+    public init(view: View, imageTransformer: @escaping (Data) -> Image?) {
+        self.view = view
+        self.imageTransformer = imageTransformer
+    }
+    
+    public func didStartLoadingImageData(for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: true,
+            shouldRetry: false))
+    }
+    
+    
+    public func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+        guard let _ = imageTransformer(data) else {
+            return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
+        }
+        
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: imageTransformer(data),
+            isLoading: false,
+            shouldRetry: false))
+    }
+    
+    public func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
+        view.display(FeedImageViewModel(
+            description: model.description,
+            location: model.location,
+            image: nil,
+            isLoading: false,
+            shouldRetry: true))
+    }
+}
