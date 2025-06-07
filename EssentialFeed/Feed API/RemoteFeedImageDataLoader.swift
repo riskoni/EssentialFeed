338cf8dbb76d682b945e5414162f7f85947c85d8
Nikolay Riskov <nriskov@abrites.com>
2025-06-07 22:20:31 +0300

    Remove @discardable result for loadImageData

--- EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -19,23 +19,17 @@ public class RemoteFeedImageDataLoader: FeedImageDataLoader {
         case connectivity
     }
     
-    @discardableResult
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         let task = HTTPTaskWrapper(completion)
         task.wrapped = client.get(from: url) { [weak self] result in
             guard self != nil else { return }
             
-            switch result {
-            case let .success((data, response)):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            case .failure:
-                task.complete(with: .failure(Error.connectivity))
-                
-            }
+            task.complete(with: result
+                .mapError { _ in Error.connectivity }
+                .flatMap{ (data, response) in
+                    let isValid = response.statusCode == 200 && !data.isEmpty
+                    return isValid ? .success(data) : .failure(Error.invalidData)
+                })
         }
         return task
     }
