256073998485603acd6ea35f754913eebf21b35d
Nikolay Riskov <nriskov@abrites.com>
2025-06-07 22:10:53 +0300

    Extract `RemoveFeedImageDataLaoder` to its own file

--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -8,59 +8,6 @@
 import XCTest
 import EssentialFeed
 
-private final class HTTPTaskWrapper: FeedImageDataLoaderTask {
-    var wrapped: HTTPClientTask?
-    private var completion: ((FeedImageDataLoader.Result) -> Void)?
-
-    init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-        self.completion = completion
-    }
-    
-    func complete(with result: FeedImageDataLoader.Result) {
-        completion?(result)
-    }
-    
-    func cancel() {
-        preventFurtherCompletions()
-        wrapped?.cancel()
-    }
-    private func preventFurtherCompletions() {
-        completion = nil
-    }
-    
-}
-
-class RemoteFeedImageDataLoader {
-    private let client: HTTPClient
-    
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    public enum Error: Swift.Error {
-        case invalidData
-    }
-    
-    @discardableResult
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = HTTPTaskWrapper(completion)
-        task.wrapped = client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case let .success((data, response)):
-                if response.statusCode == 200, !data.isEmpty {
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            case let .failure(error): task.complete(with: .failure(error))
-            }
-        }
-        return task
-    }
-}
-
 class RemoteFeedImageDataLoaderTests: XCTestCase {
     
     func test_init_doesNotPerformAnyURLRequest() {
