a97c3d547436a9c7f26edb7e5018bbfc2d752996
Nikolay Riskov <nriskov@abrites.com>
2025-06-07 23:13:14 +0300

    Rename test case class to clarify intent

--- EssentialFeedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  LocalFeedImageDataLoaderTests.swift
+//  LoadFeedImageDataFromCacheUseCaseTests.swift
 //  EssentialFeedTests
 //
 //  Created by Nikolay Riskov on 7.06.25.
@@ -9,7 +9,7 @@ import XCTest
 import EssentialFeed
 
 
-class LocalFeedImageDataLoaderTests: XCTestCase {
+class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
 
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
