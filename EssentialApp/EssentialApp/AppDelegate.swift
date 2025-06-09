676738aa5ca047420cfeca49aa7ac3b99c6c0177
Nikolay Riskov <nriskov@abrites.com>
2025-06-09 22:31:55 +0300

    Move EssentailFeed into a folder, add EssentialApp and xcworkspace file

---
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  EssentialApp
+//
+//  Created by Nikolay Riskov on 08.06.25.
+//
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {}
