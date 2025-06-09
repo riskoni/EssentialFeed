e4fb04d368a4ad9aded4e1c6cd49524e85bef894
Nikolay Riskov <nriskov@abrites.com>
2025-06-09 22:46:12 +0300

    Compose FeedViewController and show it as a root view controller

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import EssentialFeed
+import EssentialFeediOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	var window: UIWindow?
@@ -13,6 +15,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let _ = (scene as? UIWindowScene) else { return }
 		
-		
+        
+        let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
+        let imageLoader = RemoteFeedImageDataLoader(client: client)
+        let feedLoader = RemoteFeedLoader(url: url, client: client)
+        
+        let feedViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: feedLoader,
+            imageLoader: imageLoader)
+        
+        window?.rootViewController = feedViewController
 	}
 }
