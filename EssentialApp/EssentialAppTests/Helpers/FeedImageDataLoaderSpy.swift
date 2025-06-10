be902da52e5c53412accf5c0005bccdce6922e63
Nikolay Riskov <nriskov@abrites.com>
2025-06-10 23:55:52 +0300

    Extract FeedImageDataLoaderSpy into a shared scope to remove duplication

---
+++ EssentialApp/EssentialAppTests/Helpers/FeedImageDataLoaderSpy.swift
@@ -0,0 +1,40 @@
+//
+//  FeedImageDataLoaderSpy.swift
+//  EssentialApp
+//
+//  Created by Nikolay Riskov on 10.06.25.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import EssentialFeed
+
+class FeedImageDataLoaderSpy: FeedImageDataLoader {
+    private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+    
+    private(set) var cancelledURLs = [URL]()
+    
+    public var loadedURLs: [URL] {
+        return messages.map { $0.url }
+    }
+    
+    private struct Task: FeedImageDataLoaderTask {
+        let callback: () -> Void
+        func cancel() { callback() }
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        messages.append((url, completion))
+        return Task { [weak self] in
+            self?.cancelledURLs.append(url)
+        }
+    }
+    
+    public func complete(with error: Error, at index: Int = 0) {
+        messages[index].completion(.failure(error))
+    }
+    
+    public func complete(with data: Data, at index: Int = 0) {
+        messages[index].completion(.success(data))
+    }
+}
