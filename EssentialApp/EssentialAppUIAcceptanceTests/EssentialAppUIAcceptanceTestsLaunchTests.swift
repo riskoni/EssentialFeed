c02f94f4ddc2b0e92a00c22ec0919f9ba18ac353
Nikolay Riskov <nriskov@abrites.com>
2025-06-24 22:43:40 +0300

    Add UI Test target for running high-level Acceptance Tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTestsLaunchTests.swift
@@ -0,0 +1,34 @@
+//
+//  EssentialAppUIAcceptanceTestsLaunchTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Nikolay Riskov on 24.06.25.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
