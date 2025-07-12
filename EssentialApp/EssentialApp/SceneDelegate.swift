7641cafaef9af9adf5c6fc69517bf112a6ec1703
Nikolay Riskov <nriskov@abrites.com>
2025-07-12 15:01:32 +0300

    Show comments on image selection

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -30,8 +30,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         LocalFeedLoader(store: store, currentDate: Date.init)
     }()
     
-    let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+    private lazy var baseURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed")!
     
+    private lazy var navigationController = UINavigationController(
+        rootViewController: FeedUIComposer.feedComposedWith(
+            feedLoader: makeRemoteFeedLoaderWithLocalFallback,
+            imageLoader: makeLocalImageLoaderWithRemoteFallback,
+            selection: showComments))
+   
     convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
         self.init()
         self.httpClient = httpClient
@@ -47,22 +53,31 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func configureWindow() {
-        let feedViewController = UINavigationController(rootViewController:
-            FeedUIComposer.feedComposedWith(
-                feedLoader: makeRemoteFeedLoaderWithLocalFallback,
-                imageLoader: makeLocalImageLoaderWithRemoteFallback
-            )
-        )
-        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
-        window?.rootViewController = feedViewController
 	}
+    
+    private func showComments(for image: FeedImage) {
+        let url = baseURL.appendingPathComponent("/v1/image/\(image.id)/comments")
+        let comments = CommentsUIComposer.commentsComposedWith(commentsLoader: makeRemoteCommentsLoader(url: url))
+        navigationController.pushViewController(comments, animated: true)
+    }
+    
+    private func makeRemoteCommentsLoader(url: URL) -> () -> AnyPublisher<[ImageComment], Error> {
+        return { [httpClient] in
+            return httpClient
+                .getPublisher(url: url)
+                .tryMap(ImageCommentsMapper.map)
+                .eraseToAnyPublisher()
+        }
+    }
     
     func sceneWillResignActive(_ scene: UIScene) {
         localFeedLoader.validateCache { _ in }
     }
     
     private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<[FeedImage], Error> {
+        let remoteURL = baseURL.appendingPathComponent("/v1/feed")
         
         return httpClient
             .getPublisher(url: remoteURL)
--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -12,15 +12,15 @@ import EssentialFeediOS
 @testable import EssentialApp
 
 class FeedAcceptanceTests: XCTestCase {
-
+    
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let feed = launch(httpClient: .online(response), store: .empty)
-
+        
         XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 2)
         XCTAssertEqual(feed.renderedFeedImageData(at: 0), makeImageData())
         XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData())
     }
-
+    
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let sharedStore = InMemoryFeedStore.empty
         let onlineFeed = launch(httpClient: .online(response), store: sharedStore)
@@ -33,13 +33,13 @@ class FeedAcceptanceTests: XCTestCase {
         XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 0), makeImageData())
         XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 1), makeImageData())
     }
-
+    
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
         let feed = launch(httpClient: .offline, store: .empty)
-
+        
         XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 0)
     }
-
+    
     func test_onEnteringBackground_deletesExpiredFeedCache() {
         let store = InMemoryFeedStore.withExpiredFeedCache
         
@@ -56,8 +56,15 @@ class FeedAcceptanceTests: XCTestCase {
         XCTAssertNotNil(store.feedCache, "Expected to keep non-expired cache")
     }
     
+    func test_onFeedImageSelection_displaysComments() {
+        let comments = showCommentsForFirstImage()
+        
+        XCTAssertEqual(comments.numberOfRenderedComments(), 1)
+        XCTAssertEqual(comments.commentMessage(at: 0), makeCommentMessage())
+    }
+    
     // MARK: - Helpers
-
+    
     private func launch(
         httpClient: HTTPClientStub = .offline,
         store: InMemoryFeedStore = .empty
@@ -65,47 +72,106 @@ class FeedAcceptanceTests: XCTestCase {
         let sut = SceneDelegate(httpClient: httpClient, store: store)
         sut.window = UIWindow()
         sut.configureWindow()
-
+        
         let nav = sut.window?.rootViewController as? UINavigationController
-        let vc = nav?.topViewController as! ListViewController
-        vc.simulateAppearance()
-        return vc
+        return nav?.topViewController as! ListViewController
     }
     
     private func enterBackground(with store: InMemoryFeedStore) {
         let sut = SceneDelegate(httpClient: HTTPClientStub.offline, store: store)
         sut.sceneWillResignActive(UIApplication.shared.connectedScenes.first!)
     }
-
+    
+    private func showCommentsForFirstImage() -> ListViewController {
+        let feed = launch(httpClient: .online(response), store: .empty)
+        
+        feed.simulateTapOnFeedImage(at: 0)
+        RunLoop.current.run(until: Date())
+        
+        let nav = feed.navigationController
+        let vc = nav?.topViewController as! ListViewController
+        vc.simulateAppearance()
+        return vc
+    }
+    
+    private func response(for url: URL) -> (Data, HTTPURLResponse) {
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        return (makeData(for: url), response)
+    }
+    
+    private func makeData(for url: URL) -> Data {
+        switch url.path {
+        case "/image-1", "/image-2":
+            return makeImageData()
+            
+        case "/essential-feed/v1/feed":
+            return makeFeedData()
+            
+        case "/essential-feed/v1/image/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/comments":
+            return makeCommentsData()
+            
+        default:
+            return Data()
+        }
+    }
+    
+    private func makeImageData() -> Data {
+        return UIImage.make(withColor: .red).pngData()!
+    }
+    
+    private func makeFeedData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": [
+            ["id": "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086", "image": "http://feed.com/image-1"],
+            ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-2"]
+        ]])
+    }
+    
+    private func makeCommentsData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": [
+            [
+                "id": UUID().uuidString,
+                "message": makeCommentMessage(),
+                "created_at": "2020-05-20T11:24:59+0000",
+                "author": [
+                    "username": "a username"
+                ]
+            ],
+        ]])
+    }
+    
+    private func makeCommentMessage() -> String {
+        "a message"
+    }
+    
     private class HTTPClientStub: HTTPClient {
         private class Task: HTTPClientTask {
             func cancel() {}
         }
-
+        
         private let stub: (URL) -> HTTPClient.Result
-
+        
         init(stub: @escaping (URL) -> HTTPClient.Result) {
             self.stub = stub
         }
-
+        
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
             completion(stub(url))
             return Task()
         }
-
+        
         static var offline: HTTPClientStub {
             HTTPClientStub(stub: { _ in .failure(NSError(domain: "offline", code: 0)) })
         }
-
+        
         static func online(_ stub: @escaping (URL) -> (Data, HTTPURLResponse)) -> HTTPClientStub {
             HTTPClientStub { url in .success(stub(url)) }
         }
     }
-
+    
     private class InMemoryFeedStore: FeedStore, FeedImageDataStore {
         private(set) var feedCache: CachedFeed?
         private var feedImageDataCache: [URL: Data] = [:]
-
+        
         private init(feedCache: CachedFeed? = nil) {
             self.feedCache = feedCache
         }
@@ -114,25 +180,25 @@ class FeedAcceptanceTests: XCTestCase {
             feedCache = nil
             completion(.success(()))
         }
-
+        
         func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
             feedCache = CachedFeed(feed: feed, timestamp: timestamp)
             completion(.success(()))
         }
-
+        
         func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
             completion(.success(feedCache))
         }
-
+        
         func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
             feedImageDataCache[url] = data
             completion(.success(()))
         }
-
+        
         func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
             completion(.success(feedImageDataCache[url]))
         }
-
+        
         static var empty: InMemoryFeedStore {
             InMemoryFeedStore()
         }
@@ -140,36 +206,9 @@ class FeedAcceptanceTests: XCTestCase {
         static var withExpiredFeedCache: InMemoryFeedStore {
             InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date.distantPast))
         }
-
+        
         static var withNonExpiredFeedCache: InMemoryFeedStore {
             InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date()))
         }
     }
-
-    private func response(for url: URL) -> (Data, HTTPURLResponse) {
-        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
-        return (makeData(for: url), response)
-    }
-
-    private func makeData(for url: URL) -> Data {
-        switch url.absoluteString {
-        case "http://image.com":
-            return makeImageData()
-
-        default:
-            return makeFeedData()
-        }
-    }
-
-    private func makeImageData() -> Data {
-        return UIImage.make(withColor: .red).pngData()!
-    }
-
-    private func makeFeedData() -> Data {
-        return try! JSONSerialization.data(withJSONObject: ["items": [
-            ["id": UUID().uuidString, "image": "http://image.com"],
-            ["id": UUID().uuidString, "image": "http://image.com"]
-        ]])
-    }
-
 }
