addc93c178cbadb6154f2f290ed602e15f4df158
Nikolay Riskov <nriskov@abrites.com>
2025-07-12 16:00:20 +0300

    Add default presentation mapper when the Resource type matches the ResourceViewModel type

--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -32,8 +32,7 @@ public final class FeedUIComposer {
                 imageLoader: { imageLoader($0).dispatchOnMainQueue() },
                 selection: selection),
             loadingView: WeakRefVirtualProxy(feedController),
-            errorView: WeakRefVirtualProxy(feedController),
-            mapper: { $0 })
+            errorView: WeakRefVirtualProxy(feedController))
         presentationAdapter.presenter = presenter
         
         return feedController
